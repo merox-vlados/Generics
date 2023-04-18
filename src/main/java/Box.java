922a6378314e5f5d78392dfc56ae6a018f23b0d4
merox-vlados <merox-vlados@mail.ru>
2023-04-18 16:26:07 +0300

    commit 18.04.2023

--- src/main/java/Box.java
+++ src/main/java/Box.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Box {
+public class Box<K, V, F> {
+    private K key;
+    private V value;
+
+    private F floatValue;
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public F getFloatValue() {
+        return floatValue;
+    }
+
+    public void setFloatValue(F floatValue) {
+        this.floatValue = floatValue;
+    }
+
+    public Box(K key, V value, F floatValue) {
+        this.key = key;
+        this.value = value;
+        this.floatValue = floatValue;
+    }
 }
--- src/test/java/BoxTest.java
+++ src/test/java/BoxTest.java
@@ -1,4 +1,17 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class BoxTest {
-  
+
+    @Test
+    public void testBox() {
+        Box<String, Integer, Float> box1 = new Box<>("string",2, 3.5f);
+        Box<String, Integer, Float> box2 = new Box<>("string",5,6.5f);
+        float resultDouble = box1.getValue() + box1.getFloatValue() + box2.getValue() + box2.getFloatValue();
+
+        assertEquals(17,resultDouble, 0.001);
+
+    }
+
 }
